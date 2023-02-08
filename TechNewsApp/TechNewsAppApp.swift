8fa267683d1a2c8e7cc4a4672b7068e2c746dc52
Nikolai Stukalo <nist2902@yandex.ru>
2023-02-08 16:32:24 +0300

    Initial Commit

---
+++ TechNewsApp/TechNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechNewsAppApp.swift
+//  TechNewsApp
+//
+//  Created by Николай Стукало on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TechNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
